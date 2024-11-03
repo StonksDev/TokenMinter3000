e78bfd1eba6b82c959f9353c1a1e4f030c2c78f8
Stonkinator <82775779+StonksDev@users.noreply.github.com>
2024-11-03 12:54:38 +0000

    remove

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './assets/tailwind.css';
-import { createApp } from 'vue';
-import App from './App.vue';
-import { Buffer } from 'buffer';
-window.Buffer = Buffer;
-createApp(App).mount('#app');
-//# sourceMappingURL=main.js.map
